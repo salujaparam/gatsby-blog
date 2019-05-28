dc400031cc694bf9183c466817b831aca0d9cf5b
param saluja <salujaparam@gmail.com>
2019-05-28 05:40:46 +0530

    edited home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,6 @@ const IndexPage = () => {
       <Layout>
         <h1>The Great Gatsby</h1>
         <h2>I'm Param, a full-stack developer living in Chennai.</h2>
-        <p>
-          Need a developer? <Link to="/contact">Contact Me</Link>
-        </p>
       </Layout>
     </div>
   )
