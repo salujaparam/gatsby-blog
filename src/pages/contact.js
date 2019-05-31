d02cc10d5867cdb6a028afd58cc434798951d825
param saluja <salujaparam@gmail.com>
2019-05-31 13:35:33 +0530

    added projects using contentful and added contact form using formspree

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,24 +2,65 @@ import React from "react"
 import Layout from "../components/layout"
 import Head from "../components/head"
 import backgroundStyles from "./background.module.scss"
+import contactStyles from "./contact.module.scss"
 
 export default function ContactPage() {
   return (
     <div className={backgroundStyles.container}>
       <Layout>
         <Head title="Contact" />
-        <h1>Contact Page</h1>
-        <p>
-          The best way to reach me at the moment is via{" "}
-          <a
-            href="https://www.linkedin.com/in/param-saluja-8a011a171/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            @param-saluja
-          </a>{" "}
-          on linkedin
-        </p>
+        <section className="contact">
+          <div className="row">
+            <div>
+              <form
+                action="https://formspree.io/salujaparam@gmail.com"
+                method="POST"
+              >
+                {/* name */}
+                <div>
+                  <label htmlFor="name">Name</label>
+                  <input
+                    type="text"
+                    className={contactStyles.input}
+                    name="name"
+                    id="name"
+                    placeholder="Your Name"
+                  />
+                </div>
+                {/* email */}
+                <div>
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="text"
+                    className={contactStyles.input}
+                    name="email"
+                    id="email"
+                    placeholder="email@email.com"
+                  />
+                </div>
+                {/* description */}
+                <div>
+                  <label htmlFor="description">Description</label>
+                  <textarea
+                    type="text"
+                    className={contactStyles.textarea}
+                    name="description"
+                    id="description"
+                    placeholder="Comments....."
+                    rows="5"
+                  />
+                </div>
+                {/* submit */}
+                <button
+                  type="submit"
+                  style={{ background: "rgba(0,0,0, 0.2)" }}
+                >
+                  submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </section>
       </Layout>
     </div>
   )
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,13 +1,44 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import Layout from "../components/layout"
 import backgroundStyles from "./background.module.scss"
+import projectStyles from "./projects.module.scss"
 
-export default function projects() {
+export default function Projects() {
+  const data = useStaticQuery(graphql`
+    {
+      allContentfulProjects {
+        edges {
+          node {
+            title
+            info {
+              json
+            }
+            id
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <div className={backgroundStyles.container}>
       <Layout>
-        <h1>Projects</h1>
-        <p>Will be uploaded soon...</p>
+        <div className={projectStyles.project}>
+          <ul className={projectStyles.styles}>
+            {data.allContentfulProjects.edges.map(edge => {
+              return (
+                <li key={edge.node.id}>
+                  <h3>{edge.node.title}</h3>
+                  <div className={projectStyles.display}>
+                    {documentToReactComponents(edge.node.info.json)}
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       </Layout>
     </div>
   )
