c09740a5fb6a867bfc7f2a737ce3aad88f40cb89
salujaparam <salujaparam@gmail.com>
2019-09-01 01:45:54 +0530

    edited navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link, graphql, useStaticQuery } from "gatsby"
 import headerStyles from "./header.module.scss"
+import {FaLinkedin, FaGithub, FaTwitterSquare, FaHackerrank, FaInstagram} from 'react-icons/fa'
 
 export default function Header() {
   const data = useStaticQuery(graphql`
@@ -21,7 +22,7 @@ export default function Header() {
           {data.site.siteMetadata.author}
         </Link>
       </h1>
-      <nav>
+      <nav className={headerStyles.navMainList}>
         <ul className={headerStyles.navList}>
           <li>
             <Link
@@ -67,6 +68,43 @@ export default function Header() {
             >
               Contact
             </Link>
+          </li>
+        </ul>
+        <ul className={headerStyles.navList}>
+          <li>
+            <a href="https://www.linkedin.com/in/param-saluja-8a011a171/" target="_blank" rel="noopener noreferrer"
+                  className={headerStyles.navItem}
+            >
+              <FaLinkedin />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/salujaparam" target="_blank" rel="noopener noreferrer"
+                  className={headerStyles.navItem}
+            >
+              <FaGithub />
+            </a>
+          </li>
+          <li>
+          <a href="https://twitter.com/param53602815" target="_blank" rel="noopener noreferrer"
+                className={headerStyles.navItem}
+            >
+              <FaTwitterSquare />
+          </a>
+          </li>
+          <li>
+          <a href="https://www.hackerrank.com/salujaparam646" target="_blank" rel="noopener noreferrer"
+                className={headerStyles.navItem}
+            >
+              <FaHackerrank />
+          </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/param.saluja/" target="_blank" rel="noopener noreferrer"
+                className={headerStyles.navItem}
+            >
+              <FaInstagram />
+            </a>
           </li>
         </ul>
       </nav>
