84c811a67188447a4ddba4f20eae14a786d5fd2d
salujaparam <salujaparam@gmail.com>
2019-09-03 10:09:58 +0530

    edited index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,15 +10,17 @@ const IndexPage = () => {
       <Head title="Home" />
       <Layout>
         <h1>Full Stack Developer</h1>
-        <p>
-          Currently an undergrad and working on the domain of full stack
-          development.I am  pursuing my B.Tech. in Computer Science
-          Engineering.I specialize in building applications specific to the
-          business needs of my clients. I have done work in software
-          development, front-end and back-end web development using various
-          libraries, mostly concerned with JavaScript.
-        </p>
-        <img src={photo} alt="Param Saluja" />
+        <div className={backgroundStyles.image}>
+          <img src={photo} alt="Param Saluja" className={backgroundStyles.img}/>
+          <p>
+            Currently an undergrad and working on the domain of full stack
+            development.I am  pursuing my B.Tech. in Computer Science
+            Engineering.I specialize in building applications specific to the
+            business needs of my clients. I have done work in software
+            development, front-end and back-end web development using various
+            libraries, mostly concerned with JavaScript.
+          </p>
+        </div>
       </Layout>
     </div>
   )
